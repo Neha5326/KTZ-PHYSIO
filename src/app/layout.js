21e5b0dc745309d2491be00bf68f45fadf03d109
Neha <manonehamalik@gmail.com>
2025-09-18 20:54:28 +0500

    preview update

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,14 +14,29 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-
 export const metadata = {
   title: "PHYSIOTHERAPY CENTER",
   description: "Physiotherapy & Rehab Center",
-   icons: {
+  icons: {
     icon: "/logo.png",
   },
+  openGraph: {
+    title: "PHYSIOTHERAPY CENTER",
+    description: "Physiotherapy & Rehab Center",
+    url: "https://ktz-physio.vercel.app/", 
+    siteName: "Khadija Tul Zahra Physio Therapy Center",
+    images: [
+      {
+        url: "/physio.png", 
+        width: 1200,
+        height: 630,
+        alt: "KTZ Physio Therapy Logo",
+      },
+    ],
+    type: "website",
+  },
 };
+
 
 export default function RootLayout({ children }) {
   return (
